4d6c870543ca36f7880c9aa08152e7d028b237ed
Rus <de.mr44@gmail.com>
2022-06-08 23:19:37 +0500

    Rearranged the files and deleted Main.storyboard

---
+++ MusicApp/Resources/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  MusicApp
+//
+//  Created by Ruslan on 07.06.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
