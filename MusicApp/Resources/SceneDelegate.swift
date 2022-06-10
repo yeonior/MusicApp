afce8d0a42d12a929d5eb0c84ecb53a310378fec
yeonior <81576794+yeonior@users.noreply.github.com>
2022-06-10 21:18:55 +0500

    Updated delegates files

--- MusicApp/Resources/SceneDelegate.swift
+++ MusicApp/Resources/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         } else {
             let navigationController = UINavigationController(rootViewController: WelcomeViewController())
             navigationController.navigationBar.prefersLargeTitles = true
-            navigationController.navigationItem.largeTitleDisplayMode = .always
+            navigationController.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
             window?.rootViewController = navigationController
         }
         
