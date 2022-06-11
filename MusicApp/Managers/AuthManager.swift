25c0aca242552e102316c64e03210b8b15b25b7c
yeonior <81576794+yeonior@users.noreply.github.com>
2022-06-11 22:31:49 +0500

    Updated AuthManager

--- MusicApp/Managers/AuthManager.swift
+++ MusicApp/Managers/AuthManager.swift
@@ -9,8 +9,21 @@ import Foundation
 
 final class AuthManager {
     
+    struct Constants {
+        static let clientID = ""
+        static let clientSecret = ""
+    }
+    
     // MARK: - Properties
     static let shared = AuthManager()
+    
+    public var signInURL: URL? {
+        let scope = "user-read-private"
+        let redirectURI = ""
+        let baseURL = "https://accounts.spotify.com/authorize"
+        let string = "\(baseURL)?response_type=code&client_id=\(Constants.clientID)&scope\(scope)=foo&redirect_uri=\(redirectURI)"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         false
